ff3c68ad497f04f47dc754778ea107a341c73aad
matthew.ricketts <matthew.ricketts@kainos.com>
2021-11-09 14:27:05 +0000

    Removed specific credentials

--- azdevagent/build.sh
+++ azdevagent/build.sh
@@ -1,5 +1,5 @@
 docker build -t azdevagent:latest .
 az login
-az acr login --name mattrsacr
-docker tag azdevagent:latest mattrsacr.azurecr.io/azdevagent
+az acr login --name <your_acr_name>
+docker tag azdevagent:latest <your_acr_name>/azdevagent
 docker push
